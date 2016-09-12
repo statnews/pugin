bba2eac3c3bf6b71a1d2745557c6021902f9eb1a
Miles Blackwood <miles.blackwood@globe.com>
2016-09-12 16:01:11 -0400

    Default implementation and stub out eslint

---
+++ index.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+
+export default function plugit( pluginName, pluginClass ) {
+	let dataName = `plugin_${pluginName}`;
+
+	$.fn[pluginName] = function( options ) {
+		return this.each( function() {
+			let $this = $( this );
+			let data = $this.data( dataName );
+
+			if ( ! data ) {
+				$this.data( dataName, ( data = new pluginClass( this, options ) ) );
+			}
+		} );
+	};
+
+}
