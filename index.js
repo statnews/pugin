2fb53769d3e52170accaa7b978ca7eb1ca4f5ba0
Miles Blackwood <miles.blackwood@globe.com>
2016-09-12 16:47:23 -0400

    Add cached element and  props before construction, merge defaults.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import $ from 'jquery';
 export default function plugit( pluginName, pluginClass ) {
 	let dataName = `plugin_${pluginName}`;
 
-	$.fn[pluginName] = function( options ) {
+	$.fn[pluginName] = function( opts ) {
 		return this.each( function() {
 			let $this = $( this );
 			let data = $this.data( dataName );
+			let options = $.extend( {}, this._defaults, opts );
+
+			pluginClass.element = this;
+			pluginClass.$element = $this;
 
 			if ( ! data ) {
 				$this.data( dataName, ( data = new pluginClass( this, options ) ) );
